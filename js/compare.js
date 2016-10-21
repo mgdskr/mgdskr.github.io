7afe8e3507d0667c89d82875de3569fc57a0acc1
mgdskr <alexandr.maximenko@gmail.com>
2016-10-21 20:12:42 +0300

    homework1

---
+++ js/compare.js
@@ -0,0 +1,7 @@
+  $(document).ready(function(){
+    $('#compare').click(function(){
+      $('#wrapper').toggleClass('task-as-bgrd');
+      $('#wrapper-inn').toggleClass('semitransparent');
+
+                                                    });
+                  });
