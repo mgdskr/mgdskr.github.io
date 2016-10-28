394ab0121209d01145cefe9beb29168dd7a39cc1
mgdskr <alexandr.maximenko@gmail.com>
2016-10-28 13:11:00 +0300

    delete all

--- js/compare.js
+++
@@ -1,7 +0,0 @@
-  $(document).ready(function(){
-    $('#compare').click(function(){
-      $('#wrapper').toggleClass('task-as-bgrd');
-      $('#wrapper-inn').toggleClass('semitransparent');
-
-                                                    });
-                  });
